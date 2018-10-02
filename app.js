7540ab67526aa2fd45b138df912f601970837e03
David Nelson <davidnelsonc5@gmail.com>
2018-10-02 09:20:29 +0000

    update environment variable

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-
 mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-// mongoose.connect("mongodb://david:1stapp@ds121373.mlab.com:21373/yelpcamp",{ useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");

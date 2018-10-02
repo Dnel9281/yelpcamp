616657209f1ba5d9bec7267b50038681d9ff8ff7
David Nelson <davidnelsonc5@gmail.com>
2018-10-02 09:11:59 +0000

    update development database url

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
-mongoose.connect("mongodb://david:1stapp@ds121373.mlab.com:21373/yelpcamp",{ useNewUrlParser: true });
-// mongodb://david:1stapp@ds121373.mlab.com:21373/yelpcamp
+console.log(process.env.DATABASEURL);
+
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+// mongoose.connect("mongodb://david:1stapp@ds121373.mlab.com:21373/yelpcamp",{ useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
